9bc3ac42f1a2039d2e572635ac5653533ab47d6d
Sohanur20 <mdsohank168@gmail.com>
2024-02-28 17:39:31 +0600

    modal added

--- phone.js
+++ phone.js
@@ -1,16 +1,16 @@
 
-const loadPhone = async (searchTexts) => {
+const loadPhone = async (searchTexts = 'oppo', isShowAll) => {
 
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchTexts}`)
 
     const data = await res.json()
     const phones = data.data;
-    //console.log(phones)
-    displayPhones(phones)
+    // console.log(phones)
+    displayPhones(phones, isShowAll)
 
 }
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
 
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = ''
@@ -19,18 +19,21 @@ const displayPhones = (phones) => {
 
     const showContainer = document.getElementById('show-all-btn');
 
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isShowAll) {
 
         showContainer.classList.remove('hidden')
     } else {
         showContainer.classList.add('hidden')
     }
 
-    phones = phones.slice(0, 12)
+    // console.log('isShow all', isShowAll)
+    if (!isShowAll) {
+        phones = phones.slice(0, 12)
+    }
 
 
     phones.forEach(phone => {
-        //
+        // console.log(phone)
         const phoneCard = document.createElement('div')
 
         phoneCard.classList = `card w-96 bg-base-100 shadow-xl grid gri`
@@ -42,9 +45,10 @@ const displayPhones = (phones) => {
 <h2 class="card-title">${phone.phone_name}</h2>
 
 <p>${phone.brand}</p>
-<p>${phone.slug}</p>
+
 <div class="card-actions">
-    <button class="btn btn-primary">Buy Now</button>
+    <button onclick="handleShowDetails('${phone.slug}');
+    my_modal_details.showModal()" class="btn btn-primary">Show Details</button>
 </div>
 </div> `;
 
@@ -53,21 +57,71 @@ const displayPhones = (phones) => {
 
     });
 
-
+    toggleSpiner(false)
 
 
 }
 
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
+    toggleSpiner(true)
 
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText)
-    loadPhone(searchText)
-
+    loadPhone(searchText, isShowAll)
 
 
 }
 
+const toggleSpiner = (isLoading) => {
 
-// loadPhone()
+    const loadingSpinner = document.getElementById('loading-spinner')
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+    } else {
+        loadingSpinner.classList.add('hidden')
+    }
+}
+
+
+const handleShowAll = () => {
+
+    handleSearch(true)
+}
+
+// card details
+
+const handleShowDetails = async (id) => {
+
+
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+
+    const data = await res.json()
+    const phone = data.data
+
+    showPhoneDetails(phone)
+}
+
+
+
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+
+    const phoneName = document.getElementById('phone-name-modal')
+    phoneName.innerText = phone.name
+
+    const showContainer = document.getElementById('container-details')
+    showContainer.innerHTML = `
+    <img  src=${phone.image} alt="">
+    <p class="py-4 text-center">${phone.brand}</p>
+    
+    `
+
+    my_modal_details.showModal()
+
+}
+
+
+
+loadPhone()
